389dbd7d212089c7f0dcd5f07b3cae10ced162eb
Archstetics <49836037+Archstetics@users.noreply.github.com>
2021-09-15 00:30:37 +0200

    PostgreSQL query Management
    Needs to get get_password() and get_user_id() fixed

---
+++ src/db_manager.py
@@ -0,0 +1,80 @@
+import psycopg2
+
+def connect_to_db():
+    try:
+        connection = psycopg2.connect(host="localhost", database="accounts", user="postgres", password="0306")
+        return connection
+    except (Exception, psycopg2.Error) as error:
+        print(error)
+
+def insert_new_account(account, userid, passwd, site_url, connection):
+    try:
+        cursor = connection.cursor()
+        insert_query = """INSERT INTO uservault (account, userid, passwd, site_url) VALUES (%s, %s, %s, %s)"""
+        records = (account, userid, passwd, site_url)
+        cursor.execute(insert_query, records)
+        connection.commit()
+        print('Account inserito.')
+    except (Exception, psycopg2.Error) as error:
+        print(error)
+
+###############################################################################################
+###############################################################################################
+
+def get_password(account, connection):
+    try:
+        cursor = connection.cursor()
+        get_password_query = """SELECT passwd FROM uservault WHERE account = %s"""
+        cursor.execute(get_password_query, account)
+        connection.commit()
+        record = cursor.fetchone()
+        print('Password is: ', record)
+    except (Exception, psycopg2.Error) as error:
+        print(error)
+
+def get_user_id(account, connection):
+    try:
+        cursor = connection.cursor()
+        get_password_query = """SELECT userid FROM uservault WHERE account = %s"""
+        cursor.execute(get_password_query, account)
+        connection.commit()
+        record = cursor.fetchone()
+        print('User ID is: ', record)
+    except (Exception, psycopg2.Error) as error:
+        print(error)
+
+###############################################################################################
+###############################################################################################
+
+def reset_url(site_url, account, connection):
+    try:
+        cursor = connection.cursor()
+        insert_query = """UPDATE uservault SET site_url = %s WHERE account = %s"""
+        records = (site_url, account)
+        cursor.execute(insert_query, records)
+        connection.commit()
+        print('URL modificato.')
+    except (Exception, psycopg2.Error) as error:
+        print(error)
+
+def reset_account(new_account, account, connection):
+    try:
+        cursor = connection.cursor()
+        insert_query = """UPDATE uservault SET account = %s WHERE account = %s"""
+        records = (new_account, account)
+        cursor.execute(insert_query, records)
+        connection.commit()
+        print('Account utente modificato')
+    except (Exception, psycopg2.Error) as error:
+        print(error)
+
+def reset_password(passwd, account, connection):
+    try:
+        cursor = connection.cursor()
+        insert_query = """UPDATE uservault SET passwd = %s WHERE account = %s"""
+        records = (passwd, account)
+        cursor.execute(insert_query, records)
+        connection.commit()
+        print('Password modificata.')
+    except (Exception, psycopg2.Error) as error:
+        print(error)
---
+++ src/main.py
@@ -0,0 +1,25 @@
+import psycopg2
+import db_manager
+import argparse
+import getpass
+
+def main():
+   # parser = argparse.ArgumentParser(description='MasterKey Password Manager: Generate secure password, .')
+    
+    connection = db_manager.connect_to_db()
+    account = "Instagram"
+    
+    db_manager.get_password(account, connection)
+    
+    """
+    account = input("Inserisci il nome del servizio (es. Facebook, Instagram): ")
+    userid = input("Inserisci lo username: ")
+    passwd = input("Inserisci la password: ")
+    site_url = input("Inserisci l'URL del sito (es. www.facebook.com, www.instagram.com): ")
+    
+    
+    db_manager.insert_new_account(account, userid, passwd, site_url, connection)
+    """
+    
+
+main()
